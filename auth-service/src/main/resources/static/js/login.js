28cafd1c5d1dee0f3f240043d34feef030ac059c
sherjeelghouse <sherjeel.ghouse@gmail.com>
2017-04-13 23:07:07 -0500

    Added FB Login Link

--- auth-service/src/main/resources/static/js/login.js
+++ auth-service/src/main/resources/static/js/login.js
@@ -4,9 +4,11 @@ angular.module("app", []).controller("home", function($http) {
     $http.get("/user").success(function(data) {
       self.user = data.userAuthentication.details.name;
       self.authenticated = true;
+      self.admin = data && data.roles && data.roles.indexOf("ROLE_ADMIN")>-1;
     }).error(function() {
       self.user = "N/A";
       self.authenticated = false;
+      self.admin = false;
     });
 
     self.logout = function() {
@@ -16,6 +18,7 @@ angular.module("app", []).controller("home", function($http) {
       }).error(function(data) {
         console.log("Logout failed")
         self.authenticated = false;
+        self.admin = false;
       });
     };
 });
--- auth-service/src/test/java/com/arity/OAuthServiceTests.java
+++ auth-service/src/test/java/com/arity/OAuthServiceTests.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AuthenticationService.class)
+@SpringApplicationConfiguration(classes = OAuthService.class)
 @WebAppConfiguration
-public class AuthenticationServiceApplicationTests {
+public class OAuthServiceTests {
 
 	@Test
 	public void contextLoads() {
