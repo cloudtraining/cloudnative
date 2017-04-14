8c9833580eef6429c7bf6c21e611b95bb0e6ce2e
sherjeelghouse <sherjeel.ghouse@gmail.com>
2017-04-13 22:01:29 -0500

    Added FB Login Link

---
+++ auth-service/src/main/resources/static/js/login.js
@@ -0,0 +1,21 @@
+angular.module("app", []).controller("home", function($http) {
+    var self = this;
+
+    $http.get("/user").success(function(data) {
+      self.user = data.userAuthentication.details.name;
+      self.authenticated = true;
+    }).error(function() {
+      self.user = "N/A";
+      self.authenticated = false;
+    });
+
+    self.logout = function() {
+      $http.post('/logout', {}).success(function() {
+        self.authenticated = false;
+        $location.path("/");
+      }).error(function(data) {
+        console.log("Logout failed")
+        self.authenticated = false;
+      });
+    };
+});
